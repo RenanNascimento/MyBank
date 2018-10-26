815e2d8b8c120904eb6d4eaa2704af6b84efd7e2
renannascimento <renanmateusbn@gmail.com>
2018-10-26 10:02:41 -0300

    Instalação dos pacotes body-parser & request

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
 
 const app = express()
 
@@ -10,11 +11,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-  app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', indexRouter)
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(createError(404)))
